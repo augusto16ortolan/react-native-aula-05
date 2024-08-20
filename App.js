2137e5e56c5f1c1bc458aa862dc6e17a4fb36ae8
Augusto <augustoortolan02@gmail.com>
2024-08-20 20:46:22 -0300

    Text input and useStaete

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
+  const [primeiroNome, setPrimeiroNome] = useState();
+  const [sobrenome, setSobrenome] = useState();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.estiloTexto}>Olá mundo!</Text>
-      <Text style={styles.estiloTexto}>Olá mundo!</Text>
-      <Text style={styles.estiloTexto}>Olá mundo!</Text>
-      <Text style={styles.estiloTexto}>Olá mundo!</Text>
+      <TextInput
+        value={primeiroNome}
+        onChangeText={setPrimeiroNome}
+        placeholder="Informe o seu primeiro nome"
+        style={styles.estiloCampoTexto}
+      />
+      <TextInput
+        value={sobrenome}
+        onChangeText={setSobrenome}
+        placeholder="Informe o seu sobrenome"
+        style={styles.estiloCampoTexto}
+      />
+      <Text style={styles.estiloTexto}>Primeiro nome: {primeiroNome}</Text>
+      <Text style={[styles.estiloTexto, { color: "black" }]}>
+        Sobrenome: {sobrenome}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,8 +37,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   estiloTexto: {
-    fontSize: 50,
+    fontSize: 20,
     fontWeight: "bold",
     color: "white",
+  },
+  estiloCampoTexto: {
+    borderWidth: 1,
+    width: "80%",
+    height: 55,
+    color: "white",
+    fontSize: 15,
+    padding: 12,
+    borderRadius: 100,
+    marginBottom: 15,
   },
 });
